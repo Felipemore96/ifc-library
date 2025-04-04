dcd455b6b6606207ba2f45c4fa26c733f5c027aa
Felipe Moreira <felipe.moreiras796@gmail.com>
2025-04-04 17:08:13 +0200

    first commit

---
+++ src/webparts/ifcDocumentViewer/components/IIfcDocumentViewerProps.ts
@@ -0,0 +1,7 @@
+export interface IIfcDocumentViewerProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/ifcDocumentViewer/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IIfcDocumentViewerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'IfcDocumentViewerWebPartStrings' {
+  const strings: IIfcDocumentViewerWebPartStrings;
+  export = strings;
+}
